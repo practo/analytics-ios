a3c774641ea46c505847ad6b00e5bee369ca33d8
Sumeet Kumar Gupta <sumeet.gupta@practo.com>
2020-03-03 13:13:43 +0530

    Conform SEGPELIntegrationFactory to SEGIntegrationFactory

--- Analytics/Classes/Internal/SEGPELIntegrationFactory.h
+++ Analytics/Classes/Internal/SEGPELIntegrationFactory.h
@@ -6,10 +6,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <SEGIntegrationFactory.h>
+#import "SEGHTTPClient.h"
+#import "SEGStorage.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface SEGPELIntegrationFactory : NSObject
+@interface SEGPELIntegrationFactory : NSObject <SEGIntegrationFactory>
+
+@property (nonatomic, strong) SEGHTTPClient *client;
+@property (nonatomic, strong) id<SEGStorage> userDefaultsStorage;
+@property (nonatomic, strong) id<SEGStorage> fileStorage;
+
+- (instancetype)initWithHTTPClient:(SEGHTTPClient *)client fileStorage:(id<SEGStorage>)fileStorage userDefaultsStorage:(id<SEGStorage>)userDefaultsStorage;
 
 @end
 
--- Analytics/Classes/Internal/SEGPELIntegrationFactory.m
+++ Analytics/Classes/Internal/SEGPELIntegrationFactory.m
@@ -6,7 +6,28 @@
 //
 
 #import "SEGPELIntegrationFactory.h"
+#import "SEGPELIntegration.h"
 
 @implementation SEGPELIntegrationFactory
+
+- (id)initWithHTTPClient:(SEGHTTPClient *)client fileStorage:(id<SEGStorage>)fileStorage userDefaultsStorage:(id<SEGStorage>)userDefaultsStorage
+{
+    if (self = [super init]) {
+        _client = client;
+        _userDefaultsStorage = userDefaultsStorage;
+        _fileStorage = fileStorage;
+    }
+    return self;
+}
+
+- (id<SEGIntegration>)createWithSettings:(NSDictionary *)settings forAnalytics:(SEGAnalytics *)analytics
+{
+    return [[SEGPELIntegration alloc] initWithAnalytics:analytics httpClient:self.client fileStorage:self.fileStorage userDefaultsStorage:self.userDefaultsStorage];
+}
+
+- (NSString *)key
+{
+    return @"PEL";
+}
 
 @end
