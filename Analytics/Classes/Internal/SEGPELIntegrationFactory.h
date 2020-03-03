3440d2f115015717406cd8e754331c3c5cbba573
Sumeet Kumar Gupta <sumeet.gupta@practo.com>
2020-03-03 13:12:16 +0530

    Create PELIntegrationFactory

---
+++ Analytics/Classes/Internal/SEGPELIntegrationFactory.h
@@ -0,0 +1,16 @@
+//
+//  SEGPELIntegrationFactory.h
+//  Analytics@Practo
+//
+//  Created by Sumeet on 03/03/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SEGPELIntegrationFactory : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Analytics/Classes/Internal/SEGPELIntegrationFactory.m
@@ -0,0 +1,12 @@
+//
+//  SEGPELIntegrationFactory.m
+//  Analytics@Practo
+//
+//  Created by Sumeet on 03/03/20.
+//
+
+#import "SEGPELIntegrationFactory.h"
+
+@implementation SEGPELIntegrationFactory
+
+@end
