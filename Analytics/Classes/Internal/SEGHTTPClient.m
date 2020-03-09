235fcfa6706de437dd58326c6a318cc624d33708
Sumeet Kumar Gupta <sumeet.gupta@practo.com>
2020-03-09 16:15:35 +0530

    Send tokens to events pipeline

--- Analytics/Classes/Internal/SEGHTTPClient.m
+++ Analytics/Classes/Internal/SEGHTTPClient.m
@@ -1,7 +1,7 @@
 #import "SEGHTTPClient.h"
 #import "NSData+SEGGZIP.h"
 #import "SEGAnalyticsUtils.h"
-
+#import <CommonCrypto/CommonDigest.h>
 
 @implementation SEGHTTPClient
 
@@ -48,13 +48,34 @@
             @"Accept-Encoding" : @"gzip",
             @"Content-Encoding" : @"gzip",
             @"Content-Type" : @"application/json",
-            @"Authorization" : [@"Basic " stringByAppendingString:[[self class] authorizationHeader:writeKey]],
             @"User-Agent" : [NSString stringWithFormat:@"analytics@practo-ios/%@", [SEGAnalytics version]],
+            @"pipeline_token": [self pipelineTokenFrom:writeKey],
+            @"auth_prefix": [self authPrefixFrom:writeKey]
         };
         session = [NSURLSession sessionWithConfiguration:config delegate:self.httpSessionDelegate delegateQueue:NULL];
         self.sessionsByWriteKey[writeKey] = session;
     }
     return session;
+}
+
+- (NSString *)pipelineTokenFrom:(NSString *)key {
+    NSString *keyToHash = @"events-pipeline" + key;
+    const char *str = keyToHash.UTF8String;
+    if (str == NULL) {
+        str = "";
+    }
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(str, (CC_LONG)strlen(str), result);
+    NSString *keyHash = [NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+                           result[0], result[1], result[2], result[3],
+                           result[4], result[5], result[6], result[7],
+                           result[8], result[9], result[10], result[11],
+                           result[12], result[13], result[14], result[15]];
+    return keyHash
+}
+
+- (NSString *)authPrefixFrom:(NSString *)key {
+    return [key substringToIndex:8];
 }
 
 - (void)dealloc
